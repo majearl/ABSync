d5e7b5e87197d5353b6a036a233a9c911ef3385f
Madison Earl <rolstmad@gmail.com>
2026-01-18 20:23:01 -0800

    Added .env

--- run.py
+++ run.py
@@ -1,1 +1,12 @@
+import requests
+from dotenv import load_dotenv
+import os
+load_dotenv()
+API_KEY=os.getenv("API_KEY")
 print("Hello, World!")
+url = "http://10.0.11.7/api/libraries"
+headers={
+    "Authorization" : f"Bearer {API_KEY}"
+    }
+response = requests.get(url, headers=headers)
+print(response.text)
