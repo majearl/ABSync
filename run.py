8e6ff16b6a119e87a17e0032d871ff49b52ba1d4
Logan Earl <learl@commerce-architects.com>
2026-01-18 20:28:08 -0800

    Formatting

--- run.py
+++ run.py
@@ -1,12 +1,14 @@
 import requests
 from dotenv import load_dotenv
 import os
+
 load_dotenv()
-API_KEY=os.getenv("API_KEY")
-print("Hello, World!")
+API_KEY = os.getenv("API_KEY")
+
 url = "http://10.0.11.7/api/libraries"
-headers={
-    "Authorization" : f"Bearer {API_KEY}"
-    }
+headers = {
+    "Authorization": f"Bearer {API_KEY}"
+}
+
 response = requests.get(url, headers=headers)
 print(response.text)
